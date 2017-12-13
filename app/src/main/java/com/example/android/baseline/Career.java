0675f85bf24441cf988490d745f69ccc4f093850
baseline <baselineankit@gmail.com>
2017-12-13 18:04:49 +0000

    blog commited by ankit
    Signed-off-by: baseline <baselineankit@gmail.com>

---
+++ app/src/main/java/com/example/android/baseline/Career.java
@@ -0,0 +1,13 @@
+package com.example.android.baseline;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Career extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_career);
+    }
+}
